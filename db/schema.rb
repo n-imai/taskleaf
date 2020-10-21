ed957a705e66e1a51a11bfeaf84481d65ed0fde0
Nobuo Imai <nobuo.imai.ni@gmail.com>
2020-10-21 10:24:40 +0900

    Create table tasks

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2020_09_27_072053) do
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
