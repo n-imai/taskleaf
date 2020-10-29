00573b5b0bc397349f4113723565cc24d88531a7
Nobuo Imai <nobuo.imai.ni@gmail.com>
2020-10-29 16:21:29 +0900

    typo

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
-  post '/login', to: 'session#create'
-  delete '/logout', to: 'session#destroy'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   namespace :admin do
     resources :users
   end
-  
+
   root to: 'tasks#index'
   resources :tasks
 end
